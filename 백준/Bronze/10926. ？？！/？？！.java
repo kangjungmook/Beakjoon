d869477a116449d8aba27579fc1fea73685ceeed
jungmook Knag <106642094+kangjungmook@users.noreply.github.com>
2023-05-05 17:41:25 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		String b = "??!";
+		
+		String a = in.next();
+		System.out.println(a + b);
+	}
+}
